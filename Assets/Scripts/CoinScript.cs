fec569c99b727d81e421ee5d7ec9cf4de6d288e5
Fynn Klöpper <fynn.kloepper@gmail.com>
2020-09-20 00:03:07 +0200

    Coins and score counter added

---
+++ Assets/Scripts/CoinScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    bool destroyed = false;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (destroyed == false)
+        {
+            destroyed = true;
+            Destroy(gameObject);
+            ScoreTextScript.coinAmount += 1;
+        }
+    }
+}
