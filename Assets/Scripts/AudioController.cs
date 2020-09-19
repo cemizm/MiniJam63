2451b57ad26e47cdd6b85a21b581bdf78870198f
Cem Basoglu <cem.basoglu@web.de>
2020-09-20 01:31:51 +0200

    add music and pitch

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public TimeController timeController;
+
+    private float max, min;
+
+    void Start()
+    {
+        max = timeController.MaxSpeed - timeController.DefaultSpeed;
+        min = timeController.MinSpeed - timeController.DefaultSpeed;
+    }
+
+    void Update()
+    {
+        float speed = timeController.WorldSpeed - timeController.DefaultSpeed;
+
+        if (speed >= 0)
+        {
+            audioSource.pitch = 1 + (speed / max * 0.2f);
+        }
+        else
+        {
+            audioSource.pitch = 1 - (speed / min * 0.1f);
+        }
+    }
+}
