93ab907f74ce506000df4b0986030a6cbe4c24c9
Cem Basoglu <cem.basoglu@web.de>
2020-09-20 16:10:17 +0200

    Stage 5+6, Reset to start

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool ResetToStart;
+
+    public GameObject ResetPosition;
+
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (ResetToStart)
+            Player.transform.position = ResetPosition.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/MoveCircleController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCircleController : MonoBehaviour
+{
+    public float Radius;
+
+    public float Speed;
+
+    public float Angle;
+
+    private Vector2 center;
+
+    public float MoveSpeed
+    {
+        get
+        {
+            return Speed * MainController.TimeController.WorldSpeed;
+        }
+    }
+
+    void Start()
+    {
+        center = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        Angle += MoveSpeed * Time.deltaTime;
+
+        var offset = new Vector2(Mathf.Sin(Angle), Mathf.Cos(Angle)) * Radius;
+        transform.position = center + offset;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, Radius);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, 0.2f);
+
+        var offset = new Vector2(Mathf.Sin(Angle), Mathf.Cos(Angle)) * Radius;
+
+        Gizmos.DrawWireSphere((Vector2)transform.position + offset, 0.2f);
+    }
+}
