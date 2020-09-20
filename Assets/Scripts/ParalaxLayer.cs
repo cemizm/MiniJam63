a347421b7fc9d450a1619779c5bc835e26b9f8d4
Cem Basoglu <cem.basoglu@web.de>
2020-09-20 23:38:51 +0200

    parallax effect

---
+++ Assets/Scripts/ParalaxLayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParalaxLayer : MonoBehaviour
+{
+    public float parallaxFactor;
+    public void Move(float delta)
+    {
+        Vector3 newPos = transform.localPosition;
+        newPos.x -= delta * parallaxFactor;
+        transform.localPosition = newPos;
+    }
+}
---
+++ Assets/Scripts/ParallaxCamera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ParallaxCamera : MonoBehaviour
+{
+    [System.Serializable]
+    public class CameraMoveEvent : UnityEvent<float> { }
+
+    public CameraMoveEvent OnCameraMove;
+
+    private float oldPosition;
+
+
+    private void Awake()
+    {
+        if (OnCameraMove == null)
+            OnCameraMove = new CameraMoveEvent();
+    }
+
+    void Start()
+    {
+        oldPosition = transform.position.x;
+    }
+    void Update()
+    {
+        if (transform.position.x != oldPosition)
+        {
+            OnCameraMove.Invoke(oldPosition - transform.position.x);
+            oldPosition = transform.position.x;
+        }
+    }
+}
