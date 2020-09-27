cba2f4525d6de773580e1d1a671eadcf41fd2720
Fynn Klöpper <fynn.kloepper@gmail.com>
2020-09-27 19:50:39 +0200

    Added particle system

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -21,6 +21,7 @@ public class CharacterController2D : MonoBehaviour
     public bool experimentalJump = true;
     public float fallMultiplier = 2.5f;
     public float lowJumpMultiplier = 2f;
+    public ParticleSystem dust; 
 
     [Header("Events")]
     [Space]
@@ -64,6 +65,7 @@ public class CharacterController2D : MonoBehaviour
             {
                 m_Grounded = true;
                 if (!wasGrounded)
+                    CreateDust();
                     OnLandEvent.Invoke();
             }
         }
@@ -80,7 +82,6 @@ public class CharacterController2D : MonoBehaviour
             Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
             // And then smoothing it out and applying it to the character
             m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
-
             // If the input is moving the player right and the player is facing left...
             if (move > 0 && !m_FacingRight)
             {
@@ -101,6 +102,7 @@ public class CharacterController2D : MonoBehaviour
             m_Grounded = false;
 
             m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+            CreateDust();
         }
     }
 
@@ -109,10 +111,16 @@ public class CharacterController2D : MonoBehaviour
     {
         // Switch the way the player is labelled as facing.
         m_FacingRight = !m_FacingRight;
+        CreateDust(); 
 
         // Multiply the player's x local scale by -1.
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
+    }
+
+    void CreateDust()
+    {
+        dust.Play(); 
     }
 }
