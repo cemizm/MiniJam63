26cf5846134e8ebcc522cd0017129dc2d9c6a764
Cem Basoglu <cem.basoglu@web.de>
2020-09-28 11:55:16 +0200

    assets add

---
+++ Assets/TestMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMove : MonoBehaviour
+{
+    public Vector3 Target;
+
+    public float MoveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, Target, MoveSpeed * Time.fixedDeltaTime);
+    }
+}
